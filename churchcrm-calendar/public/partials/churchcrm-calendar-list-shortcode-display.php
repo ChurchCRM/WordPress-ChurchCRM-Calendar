df8124ca9b98c28f8c79015628e8499b6be12f98
Charles <crossan007@gmail.com>
2017-09-08 21:37:47 -0400

    add randomized query

--- churchcrm-calendar/public/partials/churchcrm-calendar-list-shortcode-display.php
+++ churchcrm-calendar/public/partials/churchcrm-calendar-list-shortcode-display.php
@@ -15,19 +15,18 @@
  	global $crmc_sc_output;
 
     $atts = $this->churchcrm_calendar_list_shortcode_atts;
- 
-    $events = json_decode(file_get_contents(get_option('_curchcrm_server_url')."/external/calendar/events"));
+    
     $events_count_max = $this->churchcrm_calendar_list_shortcode_atts['max'];
+    $request_string = get_option('_curchcrm_server_url')."/external/calendar/events?start=".date('Y-m-d')."&max=".$events_count_max."&r=".mt_rand();
+    $events = json_decode(file_get_contents($request_string));
 
     /*
         TODO: Render the events on the page.
     */ 
     $crmc_sc_output = "<div class=\"events-list\">";
 
-    $events_count_current = 0;
         foreach ($events as $Event) 
         {
-            if ($events_count_current >= $events_count_max) { break; }
             $crmc_sc_output .= "<div class=\"event-div\">";
             $crmc_sc_output .= "<div class=\"event-div-header\"><h1>$Event->Title</h1><h2>".date("M/j/y g:i A",strtotime($Event->Start))."</h2></div>";
             $crmc_sc_output .= "<div class=\"event-div-body\"><p>$Event->Desc</p></div>";
