c0ab0daeb67224f76605d3fe3f0f1ac4af2f8c87
Charles <crossan007@gmail.com>
2018-01-06 14:09:43 -0500

    update path to Events JSON

--- churchcrm-calendar/public/partials/churchcrm-calendar-list-shortcode-display.php
+++ churchcrm-calendar/public/partials/churchcrm-calendar-list-shortcode-display.php
@@ -17,7 +17,7 @@
     $atts = $this->churchcrm_calendar_list_shortcode_atts;
     
     $events_count_max = $this->churchcrm_calendar_list_shortcode_atts['max'];
-    $request_string = get_option('_curchcrm_server_url')."/external/calendar/events?start=".date('Y-m-d')."&max=".$events_count_max."&r=".mt_rand();
+    $request_string = get_option('_curchcrm_server_url')."/api/public/calendar/events?start=".date('Y-m-d')."&max=".$events_count_max."&r=".mt_rand();
     $events = json_decode(file_get_contents($request_string));
     date_default_timezone_set(get_option('timezone_string'));
     
